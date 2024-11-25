53cac6723fbb265f1d5cb19f77ab35284095a639
William Tremblay <wt22@videotron.ca>
2024-11-25 12:00:41 -0500

    feat (partially tested): Added remaining crud operations to items api + added debugger options for express server in dev

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,49 +1,41 @@
-import { RunResult, Statement } from 'sqlite3';
 import db from './sqlite';
 import Item from '@extension/Item';
-import ErrCallback from './types/ErrCallback';
 
-export function createItem(newItem: Omit<Item, 'id'>, callback?: ErrCallback) {
+export function createItem(newItem: Omit<Item, 'id'>) {
   const { name, website, price, url, image_url: imageUrl } = newItem;
   const sql = `INSERT INTO items (name, website, price, url, image_url) 
 	VALUES (?, ?, ?, ?, ?);`;
-  db.run(sql, [name, website, price, url, imageUrl], callback);
-}
-
-export function getItems(callback?: ErrCallback): Item[] | undefined {
-  const sql = 'SELECT * FROM items;';
-  let response = undefined;
-  db.all<Item>(sql, (err, items) => {
-    callback && callback(err);
-    if (items) {
-      response = items;
-    }
-  });
+  const response = db.prepare(sql).run(name, website, price, url, imageUrl);
 
   return response;
 }
 
-export function getItem(id: number, callback?: ErrCallback): Item | undefined {
-  const sql = 'SELECT * FROM items WHERE id = ?;';
-  let response = undefined;
-  db.get<Item>(sql, [id], (err, item) => {
-    callback && callback(err);
-    if (item) {
-      response = item;
-    }
-  });
-  return;
+export function getItems(): Item[] | undefined {
+  const sql = 'SELECT * FROM items;';
+  const response = db.prepare<unknown[], Item>(sql).all();
+
+  return response;
 }
 
-export function updateItem(updatedItem: Item, callback?: ErrCallback) {
+export function getItem(id: number): Item | undefined {
+  const sql = 'SELECT * FROM items WHERE id = ?;';
+
+  return db.prepare<[Number], Item>(sql).get(id);
+}
+
+export function updateItem(updatedItem: Item) {
   const { id, name, website, price, url, image_url: imageUrl } = updatedItem;
   const sql = `UPDATE items 
 	SET name = ?, website = ?, price = ?, url = ?, image_url = ?
 	WHERE id = ?;`;
-  db.run(sql, [name, website, price, url, imageUrl, id], callback);
+
+  return db
+    .prepare<[String, String, String, String, String, Number]>(sql)
+    .run(name, website, price, url, imageUrl, id);
 }
 
-export function deleteItem(id: number, callback?: ErrCallback) {
+export function deleteItem(id: number) {
   const sql = 'DELETE FROM items WHERE id = ?;';
-  db.run(sql, [id], callback);
+
+  return db.prepare<[Number]>(sql).run(id);
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { getItems } from './api';
+import { createItem, deleteItem, getItem, getItems, updateItem } from './api';
 import ErrCallback from './types/ErrCallback';
+import Item from '@extension/Item';
 
 dotenv.config();
 
@@ -19,9 +20,83 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get('/items', (req, res) => {
-  const items = getItems(genericErrCallback);
+  const items = getItems();
   if (items) {
-    res.send(items);
+    res.status(200).json(items);
+  } else {
+    res.status(500).send();
+  }
+});
+
+app.get('/items/:id', (req, res) => {
+  const { id } = req.params;
+  const item = getItem(Number(id));
+  if (item) {
+    res.status(200).json(item);
+  } else {
+    res.status(400).send(`Invalid item id ${id}`);
+  }
+});
+
+app.post('/items', (req, res) => {
+  const { name, website, price, url, image_url } = req.body;
+  if (!name || !website || !price || !url || !image_url) {
+    res.status(400).send('Missing required fields');
+  } else {
+    const response = createItem({ name, website, price, url, image_url });
+    if (response.changes === 0) {
+      res.status(400).send('Invalid values provided');
+    } else {
+      res.status(201).send();
+    }
+  }
+});
+
+app.put('/items/:id', (req, res) => {
+  const { id } = req.params;
+
+  const currentItem = getItem(Number(id));
+  if (!currentItem) {
+    res.status(400).send(`Invalid item id ${id}`);
+    return;
+  }
+
+  const { name, website, price, url, image_url } = req.body;
+
+  const updatedItem: Item = {
+    ...currentItem,
+    name,
+    website,
+    price,
+    url,
+    image_url,
+  };
+
+  const response = updateItem(updatedItem);
+
+  if (response.changes === 0) {
+    res.status(400).send('Invalid values provided');
+  } else {
+    res.status(200).send('Item successfully updated');
+  }
+});
+
+app.delete('/items/:id', (req, res) => {
+  const { id } = req.params;
+
+  const item = getItem(Number(id));
+
+  if (!item) {
+    res.status(400).send(`Invalid item id ${id}`);
+    return;
+  }
+
+  const response = deleteItem(item.id);
+
+  if (response.changes === 0) {
+    res.status(500).send(`Internal error while trying to delete item with id ${id}`);
+  } else {
+    res.status(200).send(`Item successfully removed`);
   }
 });
 
